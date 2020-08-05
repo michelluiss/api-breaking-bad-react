7a21f44df88c1bd3c1ee62e7952f4fa6842f6de2
michelluiss <michelluis88@gmail.com>
2020-08-04 22:56:14 -0300

    listando episodios

--- src/views/Episodes/Episodes.js
+++ src/views/Episodes/Episodes.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import api from '../../services/api'
 
 export default class Episodes extends Component {
+
+  state = {
+    episodes: [],
+    meta: {}
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await api.get('episodes/')
+      this.setState({
+        episodes: response.data.slice(0, 8),
+        meta: {
+          currentPage: 1,
+          nextPage: 2,
+          total: response.data.length,
+          paginationCount: Math.round(response.data.length / 8) + 1
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
     return(
       <div className="component-episodes">
@@ -11,21 +35,27 @@ export default class Episodes extends Component {
                 <h1>Episodios</h1>
               </div>
               <div className="box-episodes">
-                <div className="episode" v-for="( character, index ) in episodeList" key="index">
-                  <div className="col-name">
-                    <h2 className="name">character</h2>
-                    <span>Season: character season - </span>
-                    <span>Episodio: character episode</span>
+                {this.state.episodes.map(episode => (
+                  <div className="episode">
+                    <div className="col-name">
+                      <h2 className="name">{episode.title}</h2>
+                      <span>Season: {episode.season} - </span>
+                      <span>Episodio: {episode.episode}</span>
+                    </div>
+                    <div className="col-date">
+                      <p>Data de estreia:</p>
+                      <p>{episode.air_date}</p>
+                    </div>
+                    <div className="col-character">
+                      <div className="name-c">Personagens: </div>
+                      {episode.characters.map(item => (
+                        <div className="name-c">{item}</div>
+                      ))
+                      }
+                    </div>
                   </div>
-                  <div className="col-date">
-                    <p>Data de estreia:</p>
-                    <p>character air_date</p>
-                  </div>
-                  <div className="col-character">
-                    <div className="name-c">Personagens:</div>
-                    <div className="name-c">item</div>
-                  </div>
-                </div>
+                ))
+                }
               </div>
             </div>
             <nav>
