5d8e9e6c80f648f1b5d66ff05ada84a8a6f83095
michelluiss <michelluis88@gmail.com>
2020-11-14 14:20:53 -0300

    iniciando configuração do reactotron

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import Routes from './routes/routes'
 import { BrowserRouter } from 'react-router-dom';
-import HeaderApp from './componets/shared/headerApp';
+
+import './config/reactotron'
 import './App.sass';
+
+import Routes from './routes/routes'
+import { Provider } from 'react-redux'
+
+import store from './store'
+
+
+import HeaderApp from './componets/shared/headerApp';
+
+console.tron.log('teste');
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <HeaderApp></HeaderApp>
-        <Routes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <HeaderApp></HeaderApp>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
